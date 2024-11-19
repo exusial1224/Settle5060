dd42c9577216f725963a7a5355a31462f493ae4b
K_Fujiwara <tky2202114@stu.o-hara.ac.jp>
2024-11-19 15:09:50 +0900

    URLの修正

--- Settle5060/src/membership/PurchaseHistorylist.java
+++ Settle5060/src/membership/PurchaseHistorylist.java
@@ -13,7 +13,7 @@ import bean.Membership;
 import bean.PurchaseExp;
 import dao.PurchaseDAO;
 
-@WebServlet("/membership/purchaselist")
+@WebServlet("/membership/Purchaselist")
 public class PurchaseHistorylist extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
