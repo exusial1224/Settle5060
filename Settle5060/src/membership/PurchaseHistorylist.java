200bdee8bdaaf6437b7449a988fb65e9117d60d0
K_Fujiwara <tky2202114@stu.o-hara.ac.jp>
2024-11-20 10:48:18 +0900

    修正

--- Settle5060/src/membership/PurchaseHistorylist.java
+++ Settle5060/src/membership/PurchaseHistorylist.java
@@ -1,6 +1,7 @@
 package membership;
 
 import java.io.IOException;
+import java.sql.Time;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,9 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import bean.Membership;
 import bean.PurchaseExp;
 import dao.PurchaseDAO;
+import dao.SlotDAO;
 
 @WebServlet("/membership/Purchaselist")
 public class PurchaseHistorylist extends HttpServlet {
+	@SuppressWarnings("null")
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             // セッションから membershipIds リストを取得
@@ -30,11 +33,19 @@ public class PurchaseHistorylist extends HttpServlet {
             // リストから最初の Membership オブジェクトを取得し、その mbr_id を使用
             int mbr_id = membershipIds.get(0).getMbr_id();
 
-            // DAO からリセール一覧を取得
+            // DAO から入場券一覧を取得
             PurchaseDAO PurchaseDAO = new PurchaseDAO();
             List<PurchaseExp> purchaseList = PurchaseDAO.getPurchaseHistory(mbr_id);
+            List<Time> times = null;
+            System.out.print("値："+purchaseList.get(0));
+            for(int i = 0; i<purchaseList.size(); i++ ){
+
+            	SlotDAO SlotDAO = new SlotDAO();
+                times.addAll(SlotDAO.getTimes(purchaseList.get(i).getSl_id()));
+            }
 
             // リクエストにリセール一覧をセットして JSP に転送
+            request.setAttribute("timesList", times);
             request.setAttribute("purchaseList", purchaseList);
             request.getRequestDispatcher("purchaseHistorylist.jsp").forward(request, response);
 
