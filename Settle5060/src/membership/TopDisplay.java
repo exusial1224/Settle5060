622da44c825937c49daedb45c4240fe5ffc05615
K_Matsumoto <aru84ken.popo@gmail.com>
2024-11-26 12:05:01 +0900

    セッション関係変更

--- Settle5060/src/membership/TopDisplay.java
+++ Settle5060/src/membership/TopDisplay.java
@@ -7,11 +7,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/membership/TopDisplay")
 public class TopDisplay extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	HttpSession session = request.getSession();
+        session.removeAttribute("timeSlots");
 
         request.getRequestDispatcher("/membership/top.jsp").forward(request, response);
     }
