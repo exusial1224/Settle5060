d84d1f1dd3dbee5f08c12ac88023c46cc04aa8f3
K_Fujiwara <tky2202114@stu.o-hara.ac.jp>
2024-11-29 12:29:13 +0900

    パス修正

--- Settle5060/src/admin/ChangeFacilityInfoComplete.java
+++ Settle5060/src/admin/ChangeFacilityInfoComplete.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 
 import dao.FacilityDAO;
 
-@WebServlet("/admin/ChangeFacilityInfo")
+@WebServlet("/admin/ChangeFacilityInfoComplete")
 public class ChangeFacilityInfoComplete extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
